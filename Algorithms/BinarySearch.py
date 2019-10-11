5c9fd50b502981599bd181d36916fc15b082f023
Basel Shoban <basel.sh@likesyria.sy>
2019-10-11 20:37:17 +0300

    Fix a spelling mistake

--- Algorithms/BinarySearch.py
+++ Algorithms/BinarySearch.py
@@ -1,14 +1,14 @@
 def binary_search(haystack, noodle):
 	low = 0
-	height = len(haystack) - 1
+	high = len(haystack) - 1
 
-	while low <= height:
-		mid = int((low + height)/2)
+	while low <= high:
+		mid = int((low + high)/2)
 		if noodle == haystack[mid]:
 			return mid
 		elif noodle > haystack[mid]:
 			low = mid + 1
 		else:
-			height = mid - 1
+			high = mid - 1
 			
 	return -1
